5251fbf3ca010fb31b2f012b1a1bf2bf6f77e75a
Sebastian Bachmann <me@free-minds.net>
2014-11-08 21:28:44 +0100

    implementing basic lcd routine and uptime timer

---
+++ gaggianator9000.ino
@@ -0,0 +1,99 @@
+// include the library code:
+#include <LiquidCrystal.h>
+#include <stdio.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+    Serial.begin(9600);
+  
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.clear();
+  
+  cli();//stop interrupts
+
+//set timer0 interrupt at 1kHz
+  TCCR0A = 0;// set entire TCCR0A register to 0
+  TCCR0B = 0;// same for TCCR0B
+  TCNT0  = 0;//initialize counter value to 0
+  // set compare match register for 1khz increments
+  OCR0A = 249;// = (16*10^6) / (1000*64) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR0A |= (1 << WGM01);
+  // Set CS01 and CS00 bits for 64 prescaler
+  TCCR0B |= (1 << CS01) | (1 << CS00);   
+  // enable timer compare interrupt
+  TIMSK0 |= (1 << OCIE0A);
+  
+  // timer1 is set to 200Hz (LCD refresh)
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+
+  OCR1A = 6250;            // compare match register 16MHz/256/100Hz
+  TCCR1B |= (1 << WGM12);   // CTC mode
+  TCCR1B |= (1 << CS12);    // 256 prescaler 
+  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+sei();//allow interrupts
+
+}
+
+int v[8] = {0,0,0,0,0,0,0,0};
+uint8_t c = 0;
+int ana_s = 0;
+
+boolean pump_on = false;
+boolean magn_on = false;
+int timer = 0;
+
+unsigned long time = 0;
+
+int timermax = 25*1000;
+
+char line0[16];
+char line1[16];
+
+
+// second row:
+// 5 chars 
+
+void loop() {
+  // read the next analog value
+  v[(c++)&7] = analogRead(4);
+  
+  if(c == 0){
+    int x = (v[0]+v[1]+v[2]+v[3]+v[4]+v[5]+v[6]+v[7]) >> 3;
+
+    int hours = (time / 1000) / 60 / 60;
+  int minutes = (time / 1000) / 60;
+  int seconds = (time / 1000) % 60;  
+  sprintf(line0, "U: %02u:%02u:%02u", hours, minutes, seconds);
+  sprintf(line1, "P: %04u", x);
+
+  
+    if(x > 330){
+      pump_on = true;
+    }
+    else{
+      pump_on = false;
+    }
+  }
+ 
+}
+
+ISR(TIMER1_COMPA_vect){
+
+  
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.write(line0);
+  lcd.setCursor(0,1);
+  lcd.write(line1);
+
+}
+ISR(TIMER0_COMPA_vect){  // our own millis counter
+   time++;
+}
